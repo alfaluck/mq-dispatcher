294e20df0c885f047ef1ec3a0d6eb8a9f4cc0e91
Anton Shedlovsky <alfaluck@gmail.com>
2016-08-14 17:01:05 +0300

    Added GuzzleHTTP and PHPMailer libs.

--- dispatcher/vendor/composer/autoload_static.php
+++ dispatcher/vendor/composer/autoload_static.php
@@ -52,11 +52,23 @@ class ComposerStaticInit67803632ea8c69706aae5f0534105d24
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit67803632ea8c69706aae5f0534105d24::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit67803632ea8c69706aae5f0534105d24::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit67803632ea8c69706aae5f0534105d24::$classMap;
 
         }, null, ClassLoader::class);
     }
